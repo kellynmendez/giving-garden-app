bcec9e79a9346343d64fa7f7986b32fb328c2336
Kellyn Mendez <kpm.mendez@gmail.com>
2024-04-01 12:30:58 -0500

    finished about page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
@@ -14,6 +15,7 @@ function App() {
     <Router>
       <Navigation />
       <Switch>
+        <Route path="/about" component={About} />
         <Route exact path="/" component={Home} />
         <Route path="/profile" component={Profile} />
       </Switch> 
